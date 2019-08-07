9829ea7e798d3a37f34b773291c27f3611140a72
codenation <codenation@codenations-MacBook-Pro.local>
2019-08-07 17:11:04 +0100

    added css and end condition

--- script.js
+++ script.js
@@ -19,11 +19,14 @@ const next = document.querySelector(".next");
 const hide = document.querySelector(".hide");
 const outcome = document.querySelector(".outcome");
 const handsize = document.querySelector(".handsize")
+const image1 = document.querySelector(".image1");
+const image2 = document.querySelector(".image2");
+const win = document.querySelector(".win");
 
 hide.style.display = "none"
 
 class Card{
-    constructor(name,  intelligence, strength, speed, durability, energyProjection, fightingSkills){
+    constructor(name,  intelligence, strength, speed, durability, energyProjection, fightingSkills, link){
         this._name = name;
         this._intelligence = intelligence;
         this._strength = strength;
@@ -31,6 +34,7 @@ class Card{
         this._durability = durability;
         this._energyProjection = energyProjection;
         this._fightingSkills = fightingSkills;
+        this._link = link;
     }
     
     get name(){
@@ -54,38 +58,41 @@ class Card{
     get fightingSkills(){
         return this._fightingSkills;
     } 
+    get link(){
+        return this._link;
+    }
 }
 
-const iceman = new Card("Iceman", 2, 3, 3, 4, 5, 4)
-const toad = new Card("Toad", 3, 4, 3, 3, 1, 3)
-const magneto = new Card("Magneto", 5, 2, 5, 2, 6, 4)
-const deadpool = new Card("Deadpool", 2, 4, 7, 4, 1, 6)
-const mystique = new Card("Mystique", 4, 2, 2, 4, 1, 5)
-const rogue = new Card("Rogue", 3, 5, 5, 6, 1, 3)
-const juggernaut = new Card("Juggernaut", 2, 7, 2, 7, 1, 4)
-const apocalypse = new Card("Apocalypse", 6, 7, 5, 6, 3, 3)
-const shadowKing = new Card("Shadow King", 3, 1, 7, 7, 5, 1)
-const profX = new Card("Prof. X", 5, 2, 2, 2, 5, 3)
-const wolverine = new Card("wolverine", 2, 4, 2, 4, 1, 7)
-const cyclops = new Card("Cyclops", 3, 2, 2, 2, 5, 4)
-const phoenix = new Card("Phoenix", 3, 2, 7, 7, 7, 4)
-const nightcrawler = new Card("Nightcrawler", 3, 2, 7, 2, 1, 3)
-const colossus = new Card("Colossus", 2, 6, 2, 6, 1, 4)
-const storm = new Card("Storm", 2, 2, 3, 2, 5, 4)
-const jubilee = new Card("Jubilee", 2, 2, 2, 2, 1, 3)
-const emmaFrost = new Card("Emma Frost", 4, 4, 2, 5, 5, 3)
-const angel = new Card("Angel", 3, 2, 3, 2, 1, 4)
-const beast = new Card("Beast", 5, 4, 3, 4, 1, 4)
-const havok = new Card("Havok", 3, 2, 2, 3, 5, 3)
-const quicksilver = new Card("Quicksilver", 3, 4, 5, 3, 1, 4)
-const banshee = new Card("Banshee", 3, 2, 4, 3, 5, 4)
-const sunfire = new Card("Sunfire", 2, 2, 4, 5, 4, 4)
-const shadowcat = new Card("Shadowcat", 4, 2, 2, 3, 1, 5)
-const psylocke = new Card("Psylocke", 2, 3, 2, 3, 5, 6)
-const dazzler = new Card("Dazzler", 3, 2, 3, 3, 4, 3)
-const gambit = new Card("Gambit", 2, 2, 2, 2, 2, 4)
-const bishop = new Card("Bishop", 2, 3, 7, 3, 4, 4)
-const sabretooth = new Card("Sabretooth", 2, 3, 2, 4, 1, 6)
+const iceman = new Card("Iceman", 2, 3, 3, 4, 5, 4, "images/iceman.jpg")
+const toad = new Card("Toad", 3, 4, 3, 3, 1, 3, "images/toad.jpg")
+const magneto = new Card("Magneto", 5, 2, 5, 2, 6, 4, "images/magneto.png")
+const deadpool = new Card("Deadpool", 2, 4, 7, 4, 1, 6, "images/deadpool.jpg")
+const mystique = new Card("Mystique", 4, 2, 2, 4, 1, 5, "images/mystique.jpg")
+const rogue = new Card("Rogue", 3, 5, 5, 6, 1, 3, "images/rogue.jpg")
+const juggernaut = new Card("Juggernaut", 2, 7, 2, 7, 1, 4, "images/juggernaut.jpg")
+const apocalypse = new Card("Apocalypse", 6, 7, 5, 6, 3, 3, "images/apocalypse.jpg")
+const shadowKing = new Card("Shadow King", 3, 1, 7, 7, 5, 1, "images/shadowKing.jpg")
+const profX = new Card("Prof. X", 5, 2, 2, 2, 5, 3, "images/profX.jpg")
+const wolverine = new Card("wolverine", 2, 4, 2, 4, 1, 7, "images/wolverine.jpg")
+const cyclops = new Card("Cyclops", 3, 2, 2, 2, 5, 4, "images/cyclops.jpg")
+const phoenix = new Card("Phoenix", 3, 2, 7, 7, 7, 4, "images/phoenix.jpg")
+const nightcrawler = new Card("Nightcrawler", 3, 2, 7, 2, 1, 3, "images/nightcrawler.jpg")
+const colossus = new Card("Colossus", 2, 6, 2, 6, 1, 4, "images/colossus.jpg")
+const storm = new Card("Storm", 2, 2, 3, 2, 5, 4, "images/storm.jpg")
+const jubilee = new Card("Jubilee", 2, 2, 2, 2, 1, 3, "images/jubilee.jpg")
+const emmaFrost = new Card("Emma Frost", 4, 4, 2, 5, 5, 3, "images/emmaFrost.jpg")
+const angel = new Card("Archangel", 3, 2, 3, 2, 1, 4, "images/angel.jpg")
+const beast = new Card("Beast", 5, 4, 3, 4, 1, 4, "images/beast.png")
+const havok = new Card("Havok", 3, 2, 2, 3, 5, 3, "images/havok.jpg")
+const quicksilver = new Card("Quicksilver", 3, 4, 5, 3, 1, 4, "images/quicksilver.jpg")
+const banshee = new Card("Banshee", 3, 2, 4, 3, 5, 4, "images/banshee.jpg")
+const sunfire = new Card("Sunfire", 2, 2, 4, 5, 4, 4, "images/sunfire.jpg")
+const shadowcat = new Card("Shadowcat", 4, 2, 2, 3, 1, 5, "images/shadowcat.jpg")
+const psylocke = new Card("Psylocke", 2, 3, 2, 3, 5, 6, "images/psylocke.jpg")
+const dazzler = new Card("Dazzler", 3, 2, 3, 3, 4, 3, "images/dazzler.jpg")
+const gambit = new Card("Gambit", 2, 2, 2, 2, 2, 4, "images/gambit.jpg")
+const bishop = new Card("Bishop", 2, 3, 7, 3, 4, 4, "images/bishop.jpg")
+const sabretooth = new Card("Sabretooth", 2, 3, 2, 4, 1, 6, "images/sabretooth.jpg")
 
 
 function shuffle(array) {
@@ -105,6 +112,8 @@ let playerTwoHand = characters.slice(15,31)
 let currentPlayer = playerOneHand
 let currentPlayerName ="Player 1"
 let opponentPlayer = playerTwoHand
+image1.src = playerOneHand[0].link;
+image2.src = playerTwoHand[0].link;
 
 intelligences.textContent = playerOneHand[0].intelligence;
 strengths.textContent = playerOneHand[0].strength;
@@ -160,14 +169,19 @@ select.addEventListener("click", () => {
 })
 
 next.addEventListener("click", () => {
-    compare(stat1, stat2)
+    check()
+    
 }) 
 
 function compare (playerOneStat, playerTwoStat) {
     
     let shuffleTwo = playerTwoHand[0]
     let shuffleOne = playerOneHand[0]
-    if (playerOneStat > playerTwoStat) {
+    if (playerOneStat > playerTwoStat && playerOneHand.length+drawPile.length == 29){
+        outcome.textContent = "Player One wins"
+    }
+        
+    else if (playerOneStat > playerTwoStat) {
         outcome.textContent = `player one uses ${playerOneHand[0].name} with a power of ${playerOneStat} to beat player two's ${playerTwoHand[0].name} with a power of ${playerTwoStat}`
         playerOneHand.push(shuffleOne)        
         playerOneHand.push(shuffleTwo)
@@ -201,9 +215,6 @@ function compare (playerOneStat, playerTwoStat) {
     playerOneHand.shift()
     playerTwoHand.shift()
 
-    console.log(playerOneHand)
-    console.log(playerTwoHand)
-
     intelligences.textContent = currentPlayer[0].intelligence;
     strengths.textContent = currentPlayer[0].strength;
     person.textContent = currentPlayer[0].name;
@@ -212,6 +223,8 @@ function compare (playerOneStat, playerTwoStat) {
     energyprojections.textContent = currentPlayer[0].energyProjection;
     fightingskills.textContent = currentPlayer[0].fightingSkills;
     player.textContent = currentPlayerName;
+    image1.src = currentPlayer[0].link;
+    handsize.textContent = currentPlayer.length;
 
     intelligences2.textContent = `Intelligence: ${opponentPlayer[0].intelligence}`;
     strengths2.textContent = `Strength: ${opponentPlayer[0].strength}`;
@@ -220,6 +233,21 @@ function compare (playerOneStat, playerTwoStat) {
     durabilitys2.textContent = `Durability: ${opponentPlayer[0].durability}`;
     energyprojections2.textContent = `Energy Projection: ${opponentPlayer[0].energyProjection}`;
     fightingskills2.textContent = `Fighting Skills: ${opponentPlayer[0].fightingSkills}`;
-    hide.style.display = "none"
-    handsize.textContent = currentPlayer.length;
+    hide.style.display = "none";
+    image2.src = opponentPlayer[0].link;
+
+    
+}
+
+function check () {
+    console.log(playerOneHand.length)
+    if (playerOneHand.length == 0 || playerTwoHand.length == 30){
+        outcome.textContent = "Player Two wins"
+    }
+    else if (playerTwoHand.length == 0 || playerOneHand.length == 30){
+        outcome.textContent = "Player One wins"
+    }
+    else {
+        compare(stat1, stat2)
+    }
 }
